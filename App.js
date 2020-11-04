6c3252fd5d88b55bcd8f731c9364d8a54584eb6c
Anish <anishs@yahoo.com>
2020-11-04 16:17:03 +0530

    Initial Commit of Bhagyakury App

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import Icon from 'react-native-vector-icons/Ionicons';
 import { Image, StyleSheet, Text, View } from "react-native";
 import AppIntroSlider from "react-native-app-intro-slider";
 
@@ -64,7 +65,37 @@ export default class App extends React.Component {
       </View>
     );
   };
-
+  _onDone = () => {
+    // User finished the introduction. Show real app through
+    // navigation or simply by controlling state
+    alert("done");
+    this.setState({ showRealApp: true });
+  }
+  _renderNextButton = () => {
+    return (
+      <View style={styles.buttonCircle}>
+        <Icon
+          name="md-arrow-round-forward"
+          color="rgba(255, 255, 255, .9)"
+          size={24}
+        />
+      </View>
+    );
+  };
+  _renderDoneButton = () => {
+    return (
+      <View 
+      style={styles.buttonCircle}  
+     >
+      
+        <Icon
+          name="md-checkmark"
+          color="rgba(255, 255, 255, .9)"
+          size={24}
+        />
+      </View>
+    );
+  };
   render() {
     if (this.state.showHomePage){
       return <App/>
@@ -74,9 +105,12 @@ export default class App extends React.Component {
       renderItem={this._renderItem} 
       data={slides} 
       activeDotStyle={{
-        backgroundColor:"#21465b",
-        width:30
+        backgroundColor:"red",
+        // width:30
       }}
+      renderDoneButton={this._renderDoneButton } 
+      renderNextButton={this._renderNextButton}
+      onDone={this._onDone}
      />
   
     );
@@ -89,5 +123,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  buttonCircle: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'rgba(0, 0, 0, .2)',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
